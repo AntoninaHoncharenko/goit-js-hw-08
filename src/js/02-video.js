41eab5f270e7e1986fd40dba53244183fe351d65
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-01 17:19:22 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+function setTime() {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+  if (!currentTime) {
+    return;
+  }
+  player.setCurrentTime(currentTime);
+}
+
+setTime();
