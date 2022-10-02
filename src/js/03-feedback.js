87357925d2121bbc6f754b26a65a4ea67e1cf578
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-02 11:56:39 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,40 +7,27 @@ const LOCAL_STORAGE_KEY = 'feedback-form-state';
 formRef.addEventListener('input', throttle(saveDataHandler, 500));
 formRef.addEventListener('submit', submitDataHandler);
 
-// 1. привести дані до рядка і зберегти значення з форми
-
-// const formData = {};
-
 function saveDataHandler(event) {
   let formData = load(LOCAL_STORAGE_KEY);
   formData = formData ? formData : {};
 
   formData[event.target.name] = event.target.value;
 
-  //   const stringifyData = JSON.stringify(formData);
-  //   localStorage.setItem(LOCAL_STORAGE_KEY, stringifyData);
-
   save(LOCAL_STORAGE_KEY, formData);
 }
-
-// 2. розпарсити збережені дані та вивести у форму
 
 showDataHandler();
 
 function showDataHandler() {
   const formData = load(LOCAL_STORAGE_KEY);
-
   if (!formData) {
     return;
   }
 
-  //   const parseData = JSON.parse(formData);
   Object.entries(formData).forEach(([name, value]) => {
     formRef.elements[name].value = value;
   });
 }
-
-// 3. фідправити форму, очистити форму, очитстити сторедж
 
 function submitDataHandler(event) {
   event.preventDefault();
@@ -49,13 +36,8 @@ function submitDataHandler(event) {
     elements: { email, message },
   } = event.currentTarget;
 
-  console.log({
-    email: email.value,
-    message: message.value,
-  });
-  //   console.log(formData);
+  console.log({ email: email.value, message: message.value });
 
   event.currentTarget.reset();
-  //   localStorage.removeItem(LOCAL_STORAGE_KEY);
   remove(LOCAL_STORAGE_KEY);
 }
